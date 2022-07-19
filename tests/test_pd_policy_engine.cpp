f942353bc0a32ce4d411db6b2dde6fef1c5a9466
Ben V. Brown <Ralim@Ralimtek.com>
2022-07-19 20:55:04 +1000

    Update expected states to handle the new double iteration

--- tests/test_pd_policy_engine.cpp
+++ tests/test_pd_policy_engine.cpp
@@ -24,40 +24,40 @@ const uint8_t message_good_crc[]             = {FUSB_FIFO_RX_SOP, PD_MSGTYPE_GOO
 const uint8_t message_request_capabilities[] = {FUSB_FIFO_RX_SOP, PD_MSGTYPE_GET_SINK_CAP, 0, 0, 0, 0, 0};
 const uint8_t message_accept[]               = {FUSB_FIFO_RX_SOP, 0x63, 0x03, 0, 0, 0, 0}; // PS_ACCEPT
 const uint8_t mock_capabilities[]            = {FUSB_FIFO_RX_SOP,
-                                     0xA1, // Header
-                                     0x71, // Header
-                                     0x2c, // +
-                                     0x91, // | Fixed 5V @ 3A
-                                     0x01, // |
-                                     0x08, // +
-                                     0x2c, // +
-                                     0xD1, // |
-                                     0x02, // | Fixed 9V @ 3A
-                                     0x00, // +
-                                     0x2C, // +
-                                     0xB1, // |
-                                     0x04, // | Fixed 15V @ 3A
-                                     0x00, // +
-                                     0xE1, // +
-                                     0x40, // |
-                                     0x06, // | Fixed 20V @ 2.25A
-                                     0x00, // +
-                                     0x64, // +
-                                     0x21, // |
-                                     0xDC, // | PPS 3.3-11V @ 5A
-                                     0xC8, // +
-                                     0x3C, // +
-                                     0x21, // |
-                                     0x40, // | PPS 3.3-16V @ 3A
-                                     0xC9, // +
-                                     0x2D, // +
-                                     0x21, // |
-                                     0xA4, // | PPS 3.3-21V @ 2.25A
-                                     0xC9, // +
-                                     0,    // 0=CRC padding
-                                     0,
-                                     0,
-                                     0};
+                                                0xA1, // Header
+                                                0x71, // Header
+                                                0x2c, // +
+                                                0x91, // | Fixed 5V @ 3A
+                                                0x01, // |
+                                                0x08, // +
+                                                0x2c, // +
+                                                0xD1, // |
+                                                0x02, // | Fixed 9V @ 3A
+                                                0x00, // +
+                                                0x2C, // +
+                                                0xB1, // |
+                                                0x04, // | Fixed 15V @ 3A
+                                                0x00, // +
+                                                0xE1, // +
+                                                0x40, // |
+                                                0x06, // | Fixed 20V @ 2.25A
+                                                0x00, // +
+                                                0x64, // +
+                                                0x21, // |
+                                                0xDC, // | PPS 3.3-11V @ 5A
+                                                0xC8, // +
+                                                0x3C, // +
+                                                0x21, // |
+                                                0x40, // | PPS 3.3-16V @ 3A
+                                                0xC9, // +
+                                                0x2D, // +
+                                                0x21, // |
+                                                0xA4, // | PPS 3.3-21V @ 2.25A
+                                                0xC9, // +
+                                                0,    // 0=CRC padding
+                                                0,
+                                                0,
+                                                0};
 const uint8_t message_ready[]                = {FUSB_FIFO_RX_SOP, 0x66, 0x05, 0, 0, 0, 0}; // PS_READY with 0'ed CRC
 // Testing helpers
 
@@ -82,7 +82,8 @@ void iterateThoughExpectedStates(std::vector<int> expectedStates) {
   }
   pe.printStateName();
   // Check that we are at the final expected state
-  CHECK_TRUE((iterationCounter + 1) == expectedStates.size());
+
+  CHECK_EQUAL(expectedStates.size(), iterationCounter + 1);
   CHECK_EQUAL(expectedStates[iterationCounter], pe.currentStateCode());
 }
 
@@ -91,7 +92,7 @@ void test_NormalNegotiation() {
   // Next queue an actual capabilities message
 
   injectTestmessage(sizeof(mock_capabilities), mock_capabilities);
-  iterateThoughExpectedStates({6, 7, 8, 0});
+  iterateThoughExpectedStates({6, 7, 8, 0, 0});
 
   // We should now be able to pop the transmitted message out of the fifo
   const uint8_t expectedPD21VPPSMessage[] = {18, 18, 18, 19, 134, 130, 16, 45, 52, 8, 115, 255, 20, 254, 161};
@@ -106,24 +107,24 @@ void test_NormalNegotiation() {
 
   fusb_mock.setRegister(FUSB_INTERRUPTA, FUSB_INTERRUPTA_I_TXSENT);
   pe.IRQOccured();
-  iterateThoughExpectedStates({1, 0});
+  iterateThoughExpectedStates({1, 0, 0});
   fusb_mock.setRegister(FUSB_INTERRUPTA, 0);
   // Now that tx has "sent" the charger will send a good crc back
   std::cout << "Faking Good CRC" << std::endl;
 
   injectTestmessage(sizeof(message_good_crc), message_good_crc);
 
-  iterateThoughExpectedStates({2, 9, 0});
+  iterateThoughExpectedStates({2, 9, 0, 0});
   // Now the unit should be waiting for the acceptance message from the power adapter
   CHECK_EQUAL(0, pe.currentStateCode());
 
   // Now that it has sent a "good" request, respond in kind with an acceptance
 
   injectTestmessage(sizeof(message_accept), message_accept);
-  iterateThoughExpectedStates({10, 0});
+  iterateThoughExpectedStates({10, 0, 0});
   // Now send the PS_RDY
   injectTestmessage(sizeof(message_ready), message_ready);
-  iterateThoughExpectedStates({11, 12, 0});
+  iterateThoughExpectedStates({11, 12, 0, 0});
 }
 
 // Test Scenarios
@@ -134,7 +135,7 @@ TEST(PD, PDNegotiationTest) {
   CHECK_FALSE(pe.pdHasNegotiated());
   // CHECK_FALSE(pe.setupCompleteOrTimedOut(100));
   // Crank the handle until we are waiting for a pd message to come in, but with deadlock detection
-  iterateThoughExpectedStates({4, 5, 0});
+  iterateThoughExpectedStates({4, 5, 0, 0});
   // Now the thread should wait for an IRQ to signify it needs to poll data from the FUSB302
   // First load up a SOP' message into the FIFO that it will need to ignore
   injectTestmessage(sizeof(message_SOP1), message_SOP1);
@@ -147,7 +148,7 @@ TEST(PD, PDNegotiationTest) {
   // Test that the unit can be asked for its capabilities
   injectTestmessage(sizeof(message_request_capabilities), message_request_capabilities);
   // The unit will then transition to send its capabilities
-  iterateThoughExpectedStates({12, 14, 0});
+  iterateThoughExpectedStates({12, 14, 0, 0});
   // Read out the transmitted message
   const uint8_t expectedDeviceCaps[] = {18, 18, 18, 19, 142, 4, 50, 10, 144, 1, 28, 200, 64, 6, 0, 40, 200, 144, 193, 255, 20, 254, 161};
   uint8_t       sendMessage[sizeof(expectedDeviceCaps)];
@@ -160,6 +161,6 @@ TEST(PD, PDNegotiationTest) {
 
   fusb_mock.setRegister(FUSB_INTERRUPTA, FUSB_INTERRUPTA_I_TXSENT);
   pe.IRQOccured();
-  iterateThoughExpectedStates({1, 0});
+  iterateThoughExpectedStates({1, 0, 0});
   fusb_mock.setRegister(FUSB_INTERRUPTA, 0);
 }
